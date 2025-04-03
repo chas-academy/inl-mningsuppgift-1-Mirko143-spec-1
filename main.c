17e60ddc693d555bfba53af8180e4aa08d20c969
miran <miran123125@gmail.com>
2025-04-03 14:10:12 +0200

    klar

--- main.c
+++ main.c
@@ -9,5 +9,60 @@ int main()
    
     // Your code goes here
 
+    int dice_roll[100];
+
+    for (int i = 0; i < 100; i++)
+    {
+        dice_roll[i] = (rand() % 6) + 1;
+
+     //   printf("%d\n", dice_roll[i]);
+    }
+
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 0;
+    int sum = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (dice_roll[i] == 1)
+        {
+            one++;
+        }
+        if (dice_roll[i] == 2)
+        {
+            two++;
+        }
+        if (dice_roll[i] == 3)
+        {
+            three++;
+        }
+        if (dice_roll[i] == 4)
+        {
+            four++;
+        }
+        if (dice_roll[i] == 5)
+        {
+            five++;
+        }
+        if (dice_roll[i] == 6)
+        {
+            six++;
+        }
+        sum += dice_roll[i];
+    }
+
+    printf("%d\n", one);
+    printf("%d\n", two);
+    printf("%d\n", three);
+    printf("%d\n", four);
+    printf("%d\n", five);
+    printf("%d\n", six);
+    printf("%d\n", sum);
+    printf("%1.f\n", sum / 100);
+
     return 0;
 }
